5644b2f4663a15aea43030c2434f66858db18d93
JasperC13 <71346473+JasperC13@users.noreply.github.com>
2020-09-18 09:50:35 -0700

    Class Work (arrays of objects) (not done)

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -1,47 +1,45 @@
+let balls = []
 
-//create an empty array called balls
-
-let balls = [];
-
-function setup() {
-  createCanvas(800, 400);
-
+function setup(){
+  createCanvas(400,400);
+  background("orange");
 }
 
 function draw(){
-	background("pink");
-
-//	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	    balls[i].drawBall();
-      balls[i].moveBall();
-	  }
+  for (let i = 0; i < balls.length; i++){
+    balls[i].drawball();
+    bakks[i].moveball();
+  }
 }
 
-function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
-  let  b = new Ball(random(0,800), random(0,400));
+class Ball{
+
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+
+  drawBall(){
+    stroke(4);
+    ball(this.x,this.y,20,20);
+  }
+
+  moveBall(){
+    this.x=this.x+2
+    this.y=this.y+2
+  }
+}
+
+function ball(){
+  ellipse(x,y,20,20);
+}
+
+function ball1(){
+  ball(x+2.5,y+2.5)
+}
+
+function keypressed(){
+  let b = new Ball(100,100);
   balls.push(b);
-  console.log(balls);
-}
-
-//ball class from which to create new balls with similar properties.
-class Ball {
-
-	constructor(x,y){ //every ball needs an x value and a y value
-		    this.x = x;
-    		this.y = y;
-	}
-
-	drawBall(){  // draw a ball on the screen at x,y
-    		stroke(0);
-    		fill(random(0,270), random(0,270), random(0,270));
-		    ellipse(this.x,this.y,10,10);
-	}
-
-	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+random(-2,2);
-		this.y = this.y+random(-0.5,0.5);
-	}
-
-
+  print(balls);
 }
