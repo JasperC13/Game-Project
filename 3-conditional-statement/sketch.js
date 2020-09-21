e0c2eab3a08a01344cbbb133747a5281f3f89e05
JasperC13 <71346473+JasperC13@users.noreply.github.com>
2020-09-21 09:58:40 -0700

    Worksheet Conditional Statements

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -4,26 +4,37 @@ let balls = [];
 //create a variable to hold your avatar
 let me;
 
+let lives;
 
 function setup() {
   createCanvas(500, 400);
 
   //make one avatar called me
-  me = new Avatar(width/2, 300, 3);
+  me = new Avatar(width/2, 300, 3, 3);
+
+  lives=10;
 
 }
 
 function draw(){
 	background(220);
 
+  fill("white");
+  rect(0,0,75,20);
+  text("Lives: " + lives,5,15);
+
   me.drawMe();
   me.moveMe();
 
-  if (frameCount % 15 == 0) {
-      let  b = new Ball(width, random(0,height), -3);
+  if (frameCount % 25 == 0) {
+      let  b = new Ball(random(0,width), 0, 3);
       balls.push(b);
       console.log(balls); //print the balls array to the console
     }
+
+  if(lives==0){
+    noLoop();
+  }
 
 //	draw all the balls in that array
 	for (let i = 0; i < balls.length; i++) {
@@ -37,16 +48,17 @@ function draw(){
 //avatar class
 class Avatar {
 
-	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
+	constructor(x,y,xspeed,yspeed){ //every avatar needs an x value, a y value, and a speed
 		    this.x = x;
     		this.y = y;
-        this.speed = speed;
+        this.xspeed = xspeed
+        this.yspeed = yspeed;
 	}
 
 	drawMe(){  // draw the running person
-    		stroke("red");
+    		stroke("purple");
         strokeWeight(3);
-    		fill("black");
+    		fill("blue");
 		    ellipse(this.x,this.y,20,20);
         line(this.x,this.y, this.x, this.y+40);
         line(this.x, this.y+40, this.x-20, this.y+60);
@@ -54,30 +66,23 @@ class Avatar {
         line(this.x+10, this.y+50, this.x+5, this.y+60);
         line(this.x, this.y+15, this.x-10, this.y+25);
         line(this.x-10, this.y+25, this.x+10, this.y+35);
-        line(this.x+5, this.y+50,this.x+20,this.y-30);
-        line(this.x+20,this.y-30,this.x+70,this.y-20);
-        line(this.x+70,this.y-20,this.x+55,this.y+50);
-        line(this.x+55,this.y+50,this.x+10,this.y+40);
-        noFill();
-        ellipse(this.x+37.5,this.y+15,40,40);
-        line(this.x+20,this.y+32.5,this.x+42.5,this.y-10);
-        line(this.x+42.5,this.y-10,this.x+47.5,this.y+37.5);
-        line(this.x+20,this.y+10,this.x+55,this.y+17.5);
 	}
 
 	moveMe(){
-    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
-       this.y -= this.speed;
+    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by yspeed
+       this.y -= this.yspeed;
     }
 
-    if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
-        this.y += this.speed;
+    if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by yspeed
+        this.y += this.yspeed;
     }
-    if (keyIsDown(LEFT_ARROW)) {
-      this.x -= this.speed;
+
+    if (keyIsDown(RIGHT_ARROW)) { //if you hold the right arrow, move right by xspeed
+      this.x += this.xspeed;
     }
-    if (keyIsDown(RIGHT_ARROW)) {
-      this.x += this.speed;
+
+    if (keyIsDown(LEFT_ARROW)) { //if you hold the left arrow, move left by xspeed
+      this.x -= this.xspeed;
     }
 	}
 
@@ -92,7 +97,7 @@ class Avatar {
 class Ball {
 
 	//every ball needs an x value, a y value, and a speed
-	constructor(x,y, speed){
+	constructor(x,y,speed){
 		this.x = x;
     this.y = y;
     this.speed = speed;
@@ -100,22 +105,25 @@ class Ball {
 
 	// draw a ball on the screen at x,y
 	drawBall(){
-    stroke(0);
-    strokeWeight(1);
-    fill("yellow");
-    ellipse(this.x,this.y,10,10);
+    	stroke(0);
+      strokeWeight(1);
+    	fill(232,24,21);
+		  ellipse(this.x,this.y,10,10);
+      line(this.x,this.y-5,this.x-3,this.y-7)
+      ellipse(this.x-3,this.y-10,5,3)
 	}
 
 	//update the location of the ball, so it moves across the screen
 	moveBall(){
-		this.x = this.x+ this.speed;
-		this.y = this.y+.5;
+		this.x = this.x;
+		this.y = this.y+ this.speed;
 	}
 
 	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
   	bounceBall(){
-    		if (this.x >= me.x-15 && this.x <= me.x+70 && this.y > me.y-40 && this.y < me.y+40){
+    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
       			this.speed = -this.speed;
+          lives = lives-1
     		}
   	}
 
